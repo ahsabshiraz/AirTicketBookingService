ee76ca6587f478e7f10659a7a7d4b478e00f4eec
Shiraz <shirazkk1@gmail.com>
2023-10-24 00:05:06 +0530

    Added rabbit mq config

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const v1Routes = require('./v1');
+const v1ApiRoutes = require('./v1/index');
 
 const router = express.Router();
 
-router.use('/v1', v1Routes);
+router.use('/v1', v1ApiRoutes);
 
 module.exports = router;
